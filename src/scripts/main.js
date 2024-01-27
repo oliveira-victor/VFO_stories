ec6aa760537a77a9108402eb85852e80e73c1672
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-01-27 20:33:34 -0300

    feat: setting up header and hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('VFO Stories')
+const welcome = document.querySelector('.welcome');
+
+window.addEventListener('load', function() {
+    setTimeout(() => {
+        welcome.style.display = "block";
+
+        setTimeout(() => {
+            welcome.classList.add('fadeOut');
+
+            setTimeout(() => {
+                welcome.style.display = "none";
+            }, 2000)
+        }, 5000);
+    }, 1000);
+})
