f89d1251532599255117d50eba4791cfe48722b3
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-01-28 00:16:50 -0300

    feat: header scroll function, paper img, responsivity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+// welcome
+
 const welcome = document.querySelector('.welcome');
 
 window.addEventListener('load', function() {
@@ -13,3 +15,27 @@ window.addEventListener('load', function() {
         }, 5000);
     }, 1000);
 })
+
+// header
+
+/* const header = document.getElementsByClassName('navbar'); */
+
+window.addEventListener('scroll', function() {
+    const currentPosition = window.scrollY
+
+    if (currentPosition < 60) {
+        originalHeader();
+    } else {
+        activateHeader();
+    }
+})
+
+function activateHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('active');
+}
+
+function originalHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('active');
+}
