6a5a35084635e2e51dd9864914b43ac77caeee7b
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-01-30 12:13:02 -0300

    feat: custom audio controller

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -69,6 +69,7 @@ function displayCards(stories) {
             return story.text.slice(0, 90) + '...'
         }
 
+        document.getElementById('loading').style.display = 'none'
         storyListElement.insertAdjacentHTML('beforeend', cardStructure)
     })
 }
@@ -86,10 +87,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const modal = document.querySelector('.modal');
 
-/* closeBtn.addEventListener('click', function() {
-    modal.classList.remove('isVisible')
-}) */
-
 function closeStory() {
     modal.classList.remove('isVisible');
     stopAudio()
@@ -101,8 +98,59 @@ function openStory() {
 
 // Audio handler
 
+let isPlaying = false;
+
 const audioPlayer = document.querySelector('audio');
+const volumeBtn = document.getElementById('volume');
+
+const slider = document.querySelector('.slider');
+const currentTime = document.querySelector('.current-time');
+const totalDuration = document.querySelector('.total-duration');
+
+let trackIndex = 0;
+let updateTimer;
 
 function stopAudio() {
     audioPlayer.pause();
+    isPlaying = false;
+    playBtn.innerHTML = '<img class="sound-icon" src="./dist/images/music-play.png" alt="Play audio icon" title="Play">';
+}
+
+function playAudio() {
+    updateTimer = setInterval(setUpdate, 1000);
+    audioPlayer.play();
+    isPlaying = true;
+    playBtn.innerHTML = '<img class="sound-icon" src="./dist/images/music-pause.png" alt="Pause audio icon" title="Pause">';
+}
+
+const playBtn = document.getElementById('play');
+
+playBtn.addEventListener('click', function () {
+    isPlaying ? stopAudio() : playAudio()
+})
+
+function seekTo() {
+    let seekTo = audioPlayer.duration * (slider.value / 100);
+    audioPlayer.currentTime = seekTo;
+}
+
+function setUpdate() {
+    let seekPosition = 0;
+    if (!isNaN(audioPlayer.duration)) {
+        seekPosition = audioPlayer.currentTime * (100 / audioPlayer.duration);
+        slider.value = seekPosition;
+
+        let currentMinutes = Math.floor(audioPlayer.currentTime / 60);
+        let currentSeconds = Math.floor(audioPlayer.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(audioPlayer.duration / 60);
+        let durationSeconds = Math.floor(audioPlayer.duration - durationMinutes * 60);
+
+        if (currentSeconds < 10) {currentSeconds = '0' + currentSeconds}
+        if (durationSeconds < 10) {durationSeconds = '0' + durationSeconds}
+        if (currentMinutes < 10) {currentMinutes = '0' +  currentMinutes}
+        if (durationMinutes < 10) {durationMinutes = '0' + durationMinutes}
+
+        currentTime.textContent = currentMinutes + ':' + currentSeconds;
+        totalDuration.textContent = currentMinutes + ':' + durationSeconds;
+    }
 }
