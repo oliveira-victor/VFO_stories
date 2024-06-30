6c35ac3e76155ce244cc025d3131ba746d26feee
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-06-30 19:07:47 -0300

    scrolreveal, about img animation, new whale aniumation

---
+++ src/scripts/reveal.js
@@ -0,0 +1,39 @@
+window.scrollReveal = ScrollReveal({ reset: false });
+
+    scrollReveal.reveal('.revealMain', {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left'
+    });
+
+    scrollReveal.reveal('.revealWhale', {
+        duration: 2000,
+        distance: '220px',
+        delay: 500,
+        origin: 'bottom'
+    });
+
+    scrollReveal.reveal('.revealCloudLeft', {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left'
+    });
+
+    scrollReveal.reveal('.revealCloudRight', {
+        duration: 2000,
+        distance: '90px',
+        origin: 'right'
+    });
+
+    scrollReveal.reveal('.test', {
+        duration: 2000,
+        distance: '90px',
+        origin: 'right'
+    });
+
+/* scrollReveal.reveal('.hide', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+}); */
